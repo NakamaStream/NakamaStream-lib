671784e31baeb848d90cb745ee2638243f925090
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-11-26 23:38:18 -0600

    index.js - import the animesAll directory

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const AnimesRecent = require('./lib/AnimesRecent');
+const AnimesAll = require('./lib/AnimesAll');
 const CaptchaService = require('./lib/auth/CaptchaService');
 
 module.exports = {
   AnimesRecent,
+  AnimesAll,
   CaptchaService
 };
